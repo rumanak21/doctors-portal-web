05c57966d3ca48886fe3e43a38120be966e2c62e
rumanak21 <rumana2k20@gmail.com>
2022-06-29 02:17:54 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './pages/Shared/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
-    <button class="btn btn-accent">Button</button>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
