eff28cc89b76518546253703fc550bd6ae73b644
rumanak21 <rumana2k20@gmail.com>
2022-07-04 06:16:33 +0600

    Design updated

--- src/pages/Home/MakeAppoinment.js
+++ src/pages/Home/MakeAppoinment.js
@@ -11,7 +11,7 @@ const MakeAppoinment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-100px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1 mx-5'>
+            <div className='flex-1 mx-5 pb-5'>
                 <h3 className='text-xl text-primary font-bold py-2'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
                 <p className='text-white py-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet expedita dolorem optio atque quos nulla reprehenderit asperiores officiis mollitia, vel quibusdam ad odio praesentium iste, in sint saepe beatae non! Quaerat esse tempora rem ut eveniet quo sunt dignissimos atque?</p>
