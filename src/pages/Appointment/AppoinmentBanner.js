9fd966bb839e47e6c6715a614c1fef9d215244c1
rumanak21 <rumana2k20@gmail.com>
2022-08-06 01:48:28 +0200

    Login form with google signIn completed

--- src/pages/Appointment/AppoinmentBanner.js
+++ src/pages/Appointment/AppoinmentBanner.js
@@ -8,9 +8,9 @@ import { format } from 'date-fns';
 const AppoinmentBanner = ({date, setDate}) => {
    
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                 <div className='pr-10 '>
                     <DayPicker mode="single"
                         selected={date}
--- src/pages/Appointment/Service.js
+++ src/pages/Appointment/Service.js
@@ -3,20 +3,20 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const {name, slot} = service;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body text-center ">
-                <h2 class=" text-xl font-bold text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center ">
+                <h2 className=" text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {slot.length? <span>{slot[0]}</span>: <span className='text-red-500'>No slot available</span> }
                 </p>
                 <p>{slot.length} {slot.length>1? 'spaces': 'space'} available!</p>
-                <div class="card-actions justify-center">
+                <div className="card-actions justify-center">
 
                     <label 
                     for="booking-modal" 
                     disabled={slot.length===0} 
                     onClick= {()=>setTreatment(service)}
-                    class="btn btn-sm btn-secondary bg-gradient-to-r from-secondary to-primary uppercase text-white"
+                    className="btn btn-sm btn-secondary bg-gradient-to-r from-secondary to-primary uppercase text-white"
                     >Book Appointment</label>
                 </div>
             </div>
